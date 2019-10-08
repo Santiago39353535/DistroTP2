4e6d3f0b90773e574099a0bedb3d54403dd9178c
Santiago39353535 <santiagomarinaro1@gmail.com>
2019-10-08 17:05:36 -0300

    upload udp

--- udp_client/upload_file.py
+++ udp_client/upload_file.py
@@ -1,4 +1,64 @@
+import socket
+
+CHUNK_SIZE = 1024
+
+def recv_numeros(socket):
+	buf = ''
+	while '\n' not in buf:
+		recv, addr = socket.recvfrom(1)
+		buf += recv.decode()
+	return buf
+
+def send_ack(socket,server_address,ack):
+	val = str(ack) + '\n'
+	socket.sendto(val.encode(),server_address)
+
 def upload_file(server_address, src, name):
-  # TODO: Implementar UDP upload_file client
-  print('UDP: upload_file({}, {}, {})'.format(server_address, src, name))
-  pass
+	# TODO: Implementar UDP upload_file client
+	print('UDP: upload_file({}, {}, {})'.format(server_address, src, name))
+	
+	own_adress = ("127.0.0.1","8081")
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind(own_address)
+
+	inicio = 0
+	fin = 1
+
+	#Three-way-Handshake
+	s.sendto('inicio'.encode(),server_address)
+
+	ack_recv = recv_numero(s)
+	if( ack_recv != inicio):
+		raise Exeption("Problema ACK sincronizaciion")
+	
+
+	send_ack(s,server_address,inicio)
+
+	#Fin de la sincroizacion
+	
+	esperado = 10
+	s.sendto("upl".encode(),server_address)
+	ack_recv = recv_numero(s)
+	if( ack_recv != esperado):
+		raise Exeption("Problema ACK sincronizaciion de operacion")
+	esperado++
+	
+	f = open(src, "rb")
+	content = f.read(CHUNK_SIZE)
+
+	while content:
+		s.sendto((str(len(content))+'\n'+content).encode(),server_address)
+		ack_recv = recv_numero(s)
+		if( ack_recv == esperado):
+			esperado++
+			content = f.read(CHUNK_SIZE)
+
+	print("Informando Fin de Archivo")
+	#fin de la coneccion
+	s.sendto('fin'.encode(),server_address)
+	ack_recv = recv_numeros(s)
+	recv, addr = socket.recvfrom(3)
+	if( recv.decode() != 'fin'):
+		 raise Exeption("Problema cierre de coneccion")
+	send_ack(s,server_address,fin)
+
--- udp_server/start_server.py
+++ udp_server/start_server.py
@@ -1,4 +1,9 @@
+import socket
+
 def start_server(server_address, storage_dir):
-  # TODO: Implementar UDP server
-  print('UDP: start_server({}, {})'.format(server_address, storage_dir))
-  pass
+	# TODO: Implementar UDP server
+	print('UDP: start_server({}, {})'.format(server_address, storage_dir))
+  
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind(server_address)
+	
