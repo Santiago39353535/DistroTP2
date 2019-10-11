da3fcfded6d7f2133a50b8cbd247a8ba6c996050
Santiago39353535 <santiagomarinaro1@gmail.com>
2019-10-11 11:31:37 -0300

    upload terminado

--- udp_client/upload_file.py
+++ udp_client/upload_file.py
@@ -5,11 +5,24 @@ CHUNK_SIZE = 1024
 RECV = 1500
 #header SSSSSAAAAATTTT
 
+def mandar_mensaje(s,addr,seq,ack,tam,data):
+	msg = str(seq).zfill(5)+str(ack).zfill(5)+str(tam).zfill(4)+data
+	s.sendto(msg.encode(),addr)
+
+def recibir_mensaje(s):
+	recv, addr = s.recvfrom(RECV)
+	recv = recv.decode()
+	seq = int(recv[0:5])#del 0 al 5 sin incluir
+	ack = int(recv[5:10])
+	tam = int(recv[10:14])
+	data = recv[14:14+int(tam)]
+	return (seq,ack,tam,data)
+
 def upload_file(server_address, src, name):
 	# TODO: Implementar UDP upload_file client
 	print('UDP: upload_file({}, {}, {})'.format(server_address, src, name))
 
-	own_address = ("127.0.0.0",8079)
+	own_address = ("127.0.0.1",8081)
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	s.bind(own_address)
 	#s.settimeout(1)
@@ -30,45 +43,50 @@ def upload_file(server_address, src, name):
 
 	#Fin de la sincroizacion
 
+
+	#Mando comando
 	seq_e = 10
 	ack_e = 0
 	tam_e = 3
 	data_e = 'upl'
-	msg = str(seq_e).zfill(5)+str(ack_e).zfill(5)+str(tam_e).zfill(4)+data_e
-	s.sendto(msg.encode(),server_address)
+	mandar_mensaje(s,server_address,seq_e,ack_e,tam_e,data_e)
 
 	esperado = seq_e
-	recv, addr = s.recvfrom(RECV)
-	recv = recv.decode()
-	seq_r = int(recv[0:5])#del 0 al 5 sin incluir
-	ack_r = int(recv[5:10])
-	tam_r = int(recv[10:14])
-	data_r = recv[14:14+int(tam_r)]
+
+	seq_r, ack_r, tam_r, data_r = recibir_mensaje(s)
 
 	if( ack_r != esperado):
 		raise Exeption("Problema ACK sincronizaciion de operacion")
 	seq_e += 1
-	f = open(src, "rb")
 
+	#mando nombre del archivo
+	print(name)
+	mandar_mensaje(s,server_address,seq_e,ack_e,len(name),name)
+
+	esperado = seq_e
+
+	seq_r, ack_r, tam_r, data_r = recibir_mensaje(s)
+
+	if( ack_r != esperado):
+		raise Exeption("Problema ACK sincronizaciion de operacion")
+	seq_e += 1
+
+
+	#Empiezo a mandar archivo
+	f = open(src, "r")
+	data_e = f.read(CHUNK_SIZE)
+
+	i = 1
 	while data_e:
-		try:
+		i += 1
+		tam_e = len(data_e)
+		mandar_mensaje(s,server_address,seq_e,ack_e,tam_e,data_e)
+		esperado = seq_e
+		seq_r, ack_r, tam_r, data_r = recibir_mensaje(s)
+		if( ack_r == esperado):
+			seq_e += 1
 			data_e = f.read(CHUNK_SIZE)
-			tam_e = len(data_e)
-			msg = str(seq_e).zfill(5)+str(ack_e).zfill(5)+str(tam_e).zfill(4)+data_e
-			s.sendto(msg.encode(),server_address)
-			esperado = seq_e
 
-			recv, addr = s.recvfrom(RECV)
-			recv = recv.decode()
-			seq_r = int(recv[0:5])#del 0 al 5 sin incluir
-			ack_r = int(recv[5:10])
-			tam_r = int(recv[10:14])
-			data_r = recv[14:14+int(tam_r)]
-			if( ack_r == esperado):
-				seq_e += 1
-				data_e = f.read(CHUNK_SIZE)
-		except socket.timeout:
-			pass
 
 	print("Informando Fin de Archivo")
 
@@ -77,7 +95,6 @@ def upload_file(server_address, src, name):
 	ack_e = 0
 	tam_e = 3
 	data_e = 'fin'
-	msg = str(seq_e).zfill(5)+str(ack_e).zfill(5)+str(tam_e).zfill(4)+data_e
-	s.sendto(msg.encode(),server_address)
+	mandar_mensaje(s,server_address,seq_e,ack_e,tam_e,data_e)
 	f.close()
 	s.close()
--- udp_server/start_server.py
+++ udp_server/start_server.py
@@ -3,6 +3,39 @@ import socket
 RECV = 1500
 #header SSSSSAAAAATTTT
 
+def mandar_mensaje(s,addr,seq,ack,tam,data):
+	msg = str(seq).zfill(5)+str(ack).zfill(5)+str(tam).zfill(4)+data
+	s.sendto(msg.encode(),addr)
+
+def recibir_mensaje(s):
+	recv, addr = s.recvfrom(RECV)
+	recv = recv.decode()
+	seq = int(recv[0:5])#del 0 al 5 sin incluir
+	ack = int(recv[5:10])
+	tam = int(recv[10:14])
+	data = recv[14:14+int(tam)]
+	return (seq,ack,tam,data,addr)
+
+def upload(s,src):
+	f = open(src, "w")
+	while True:
+		seq_r, ack_r, tam_r, data_r,addr = recibir_mensaje(s)
+		#no soporta perdida de paquete
+		if data_r == 'fin':
+			break
+		seq_e = 0
+		ack_e = seq_r
+		tam_e = 0
+		data_e = ''
+		mandar_mensaje(s,addr,seq_e,ack_e,tam_e,data_e)
+		seq_e += 1
+		#perdida de paquete rompe, poner en header num de seq
+		f.write(data_r)
+	print("Termino de recibir el archivo")
+	f.close()
+
+
+
 def start_server(server_address, storage_dir):
 	# TODO: Implementar UDP server
 	print('UDP: start_server({}, {})'.format(server_address, storage_dir))
@@ -11,39 +44,28 @@ def start_server(server_address, storage_dir):
 	s.bind(server_address)
 	#s.settimeout(1)
 
-	fin = 0
-
-
-
-	recv, addr = s.recvfrom(RECV)
-	recv = recv.decode()
-	seq_r = int(recv[0:5])#del 0 al 5 sin incluir
-	ack_r = int(recv[5:10])
-	tam_r = int(recv[10:14])
-	data_r = recv[14:14+int(tam_r)]
+	#Recivo comando
+	seq_r, ack_r, tam_r, codigo, addr = recibir_mensaje(s)
 
 	seq_e = 0
 	ack_e = seq_r
 	tam_e = 0
 	data_e = ''
-	msg = str(seq_e).zfill(5)+str(ack_e).zfill(5)+str(tam_e).zfill(4)+data_e
-	s.sendto(msg,addr)
+	mandar_mensaje(s,addr,seq_e,ack_e,tam_e,data_e)
+	seq_e += 1
 
-	f = open("storage/ejempplo","wb")
+	#Recivo nombre de archivo
+	seq_r, ack_r, tam_r, nombre, addr = recibir_mensaje(s)
+	print(nombre)
+	seq_e = 0
+	ack_e = seq_r
+	tam_e = 0
+	data_e = ''
+	mandar_mensaje(s,addr,seq_e,ack_e,tam_e,data_e)
+	seq_e += 1
 
-	while True:
-			recv, addr = s.recvfrom(RECV)
-			recv = recv.decode()
-			seq_r = int(recv[0:5])#del 0 al 5 sin incluir
-			ack_r = int(recv[5:10])
-			tam_r = int(recv[10:14])
-			data_r = recv[14:14+int(tam_r)]
-			#no soporta perdida de paquete
-			if data_r == 'fin':
-				break
-			send_ack(s,addr,esperado)
-			#perdida de paquete rompe, poner en header num de seq
-			f.write(recv.decode())
 
-	f.close()
+	if codigo == "upl":
+		upload(s,storage_dir+'/'+nombre)
+
 	s.close()
